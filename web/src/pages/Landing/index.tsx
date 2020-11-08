8e8692987217f2bb7723d159ea75045a77dd2021
Dayvson Sales <dayvson@outlook.com>
2020-11-08 12:02:10 -0300

    remove feature test code
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,39 +1,16 @@
-import { userInfo } from "os";
 import React from "react";
-import { FaArrowRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg';
-import { useUserContext } from "../../contexts/UserContext";
+import logoImg from "../../assets/images/logo.svg";
 
-import './styles.css';
+import "./styles.css";
 
 export default function Landing() {
-
-  const { user, updateUser } = useUserContext();
-
-  setTimeout(() => {
-    updateUser({
-      name: "Dayvson",
-      age: 20,
-      girlfriend_name: "Maria"
-    });
-  }, 3000);
-
-  setTimeout(() => {
-    updateUser({
-      name: "Mario",
-      age: 20,
-      girlfriend_name: "Maria"
-    });
-  }, 5000);
-
   return (
     <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoImg} alt="Happy" />
-        
-        { user?.name }
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
