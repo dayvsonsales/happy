4f8d07f1fb7c7c7b0490dc626dc51a33356ee922
Dayvson Sales <dayvson@outlook.com>
2020-11-10 14:08:36 -0300

    refactor: login page
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- web/src/pages/Auth/Login/index.tsx
+++ web/src/pages/Auth/Login/index.tsx
@@ -6,26 +6,28 @@ import arrow from "../../../assets/images/arrow.svg";
 
 const Login: React.FC = () => {
   return (
-    <div className="container-auth">
-      <div className="back-button-auth">
-        <img src={arrow} alt="voltar" />
-      </div>
-      <h3 className="title-auth">Fazer login</h3>
-      <div className="form-auth">
-        <label>E-mail</label>
-        <input type="email" name="email" />
-        <label>Senha</label>
-        <input type="email" name="email" />
-      </div>
-      <div className="bottom-form-auth">
-        <div className="remember">
-          <input type="checkbox" name="remember" />
-          Lembrar-me
+    <>
+      <div className="container-auth">
+        <div className="back-button-auth">
+          <img src={arrow} alt="voltar" />
         </div>
-        <a href="#a">Esqueci minha senha</a>
+        <h3 className="title-auth">Fazer login</h3>
+        <div className="form-auth">
+          <label>E-mail</label>
+          <input type="email" name="email" />
+          <label>Senha</label>
+          <input type="email" name="email" />
+        </div>
+        <div className="bottom-form-auth">
+          <div className="remember">
+            <input type="checkbox" name="remember" />
+            Lembrar-me
+          </div>
+          <a href="#a">Esqueci minha senha</a>
+        </div>
+        <button>Entrar</button>
       </div>
-      <button>Entrar</button>
-    </div>
+    </>
   );
 };
 
