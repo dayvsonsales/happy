746d24443e8f43c93a31cf86fbb4a9f086daf9b6
Dayvson Sales <dayvson@outlook.com>
2020-11-13 22:24:08 -0300

    feat: improve orphanages map page
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,23 +1,38 @@
-import React from "react";
-import { Marker, Popup } from 'react-leaflet';
-import L from 'leaflet'
+import React, { useEffect, useState } from "react";
+import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import { Link } from "react-router-dom";
 import { FiArrowRight, FiPlus } from "react-icons/fi";
 
 import mapMarkerImg from "../../assets/images/map-marker.svg";
-import Map from '../../components/Map';
+import Map from "../../components/Map";
 
-import './styles.css';
+import "./styles.css";
+import api from "../../services/api";
+import { Orphanage } from "../../models/Orphanage";
+
+import ReactLeafletSearch from "react-leaflet-search";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
-
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
-})
+  popupAnchor: [170, 2],
+});
 
 export default function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState([] as Orphanage[]);
+
+  useEffect(() => {
+    async function loadOrphanages() {
+      const { data } = await api.get("/orphanages?pending=false");
+
+      setOrphanages(data);
+    }
+
+    loadOrphanages();
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -29,20 +44,41 @@ export default function OrphanagesMap() {
         </header>
 
         <footer>
-          <strong>Rio do Sul</strong>
-          <span>Santa Catarina</span>
+          <strong>Escolha uma cidade</strong>
+          <span>Brasil</span>
         </footer>
       </aside>
 
-      <Map>
-        <Marker icon={happyMapIcon} position={[-27.2092052,-49.6401092]}>
-          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-            Lar das meninas
-            <Link to={`/orphanages/1`}>
-              <FiArrowRight size={20} color="#fff" />
-            </Link>
-          </Popup>
-        </Marker>
+      <Map boxZoom={true} zoomControl={true}>
+        <ReactLeafletSearch
+          position="topleft"
+          inputPlaceholder="Search a location"
+          showMarker={false}
+          zoom={15}
+          closeResultsOnClick={true}
+          openSearchOnLoad={false}
+          className="search-custom-style"
+        >
+          {(info) => <></>}
+        </ReactLeafletSearch>
+        {orphanages.map((orphanage) => (
+          <Marker
+            icon={happyMapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
       <Link to="/orphanages/create" className="create-orphanage">
@@ -51,5 +87,3 @@ export default function OrphanagesMap() {
     </div>
   );
 }
-
-// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
