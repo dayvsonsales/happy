79f5308a2edf6784682a913d2b345393c3242665
Dayvson Sales <dayvson@outlook.com>
2020-11-14 14:56:22 -0300

    feat: add alert options to done component
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

--- web/src/components/Actions/Done/index.tsx
+++ web/src/components/Actions/Done/index.tsx
@@ -4,16 +4,17 @@ import "./styles.css";
 
 import doneIcon from "../../../assets/images/done-icon.svg";
 
-const Done: React.FC = () => {
+const Done: React.FC<{
+  alertMessage: string;
+  alertButtonMessage: string;
+  callback: any;
+}> = ({ alertMessage, alertButtonMessage, callback = () => {} }) => {
   return (
     <div className="container-delete">
       <div className="message-container">
         <p className="title">Ebaa!</p>
-        <p className="subtitle">
-          O cadastro deu certo e foi enviado ao administrador para ser aprovado.
-          Agora é só esperar :)
-        </p>
-        <button>Voltar para o mapa</button>
+        <p className="subtitle">{alertMessage}</p>
+        <button onClick={(e) => callback(e)}>{alertButtonMessage}</button>
       </div>
       <div>
         <img className="icon" src={doneIcon} alt="Done" />
